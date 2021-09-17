8fd2adea137b4f88b7609a8a45e70744e19bec79
Khalid Saifullah <khalid277144@gmail.com>
2021-09-18 04:12:02 +0600

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(install_requires=required)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="BERTify",
+    version="0.0.1",
+    description="BERT embedding extractor for bengali / english data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    author="Khalid Saifullah",
+    author_email="khalidsaifullaah.github.io",
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python'
+    ],
+    install_requires=required,
+    package_data={'': ['*']},
+    python_requires='>=3.5',
+)
