4481ef94144cd8bade84d82ada78e0482b9660bc
Khalid Saifullah <khalid277144@gmail.com>
2021-09-18 04:20:33 +0600

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
-from setuptools import setup, find_packages
+import pip
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+links = []
+requires = []
+
+try:
+    requirements = pip.req.parse_requirements('requirements.txt')
+except:
+    # new versions of pip requires a session
+    requirements = pip.req.parse_requirements(
+        'requirements.txt', session=pip.download.PipSession())
+
+for item in requirements:
+    # we want to handle package names and also repo urls
+    if getattr(item, 'url', None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, 'link', None): # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))
 
 setup(
     name="BERTify",
@@ -12,18 +33,12 @@ setup(
     description="BERT embedding extractor for bengali / english data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url='https://github.com/khalidsaifullaah/BERTify',
+    license='MIT',
     packages=find_packages(),
-    author="Khalid Saifullah",
-    author_email="khalidsaifullaah.github.io",
-    classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python'
-    ],
-    install_requires=required,
-    package_data={'': ['*']},
-    python_requires='>=3.5',
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=requires,
+    dependency_links=links
 )
